7a3e21d07b9910e4c4d3677eccf8688fda191a97
saliko <saliko.semo@gmail.com>
2025-08-09 19:52:53 +0800

    update

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ syncthing for kernelsu
 # upStream
 
 [![](https://docs.syncthing.net/_static/logo-horizontal.svg)](https://github.com/syncthing/syncthing)
+
+# how to use
+input http://127.0.0.1:8384 in browser.
--- boot-completed.sh
+++ boot-completed.sh
@@ -1,9 +1,11 @@
-#
 ASH_STANDALONE=1
 
 MODDIR=${0%/*}
 SYNCTHING_BIN="${MODDIR}/bin/syncthing"
+SYNCTHING_LOG="${MODDIR}/log.txt"
 export XDG_STATE_HOME="${MODDIR}"
 export HOME="${MODDIR}"
 
+# flush logs
+rm -f $SYNCTHING_LOG
 $SYNCTHING_BIN &> ${MODDIR}/log.txt
